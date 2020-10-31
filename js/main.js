3579a1fe86144d900d252f6fd749cfe195cfdc02
Nathannael <nathannael2009@gmail.com>
2020-10-31 12:23:54 -0300

    Improve display on mobile and update text

--- js/main.js
+++ js/main.js
@@ -1,26 +1,28 @@
-json_en = { "about":"about", "greeting":"Hi, I'm Nathann!", "desc":"I'm 22 years old, and I'm a web developer. I like all that things that go beep-boop, so in my free time I'll probably be reading something about technology, studying something related to software development (I was just watching stuff on Elixir and Go the other day) or listening to electronic music. Feel free to say hi on my socials on the main page!"};
-json_pt = { "about":"sobre", "greeting":"Olá, meu nome é Nathann!", "desc":"Tenho 22 anos e sou desenvolvedor web. Sou um entusiasta de tudo que tenha a ver com computadores, então no meu tempo livre me divido entre ler artigos e ouvir podcasts sobre tecnologia, estudar algo relacionado a programação (esses dias estava fazendo uns vídeos de Elixir e Go), e escutar música eletrônica. Sinta-se livre para entrar em contato comigo nas minhas redes sociais que estão na página principal!"};
+document.getElementById('closebtn').addEventListener('click', closeNav);
+document.getElementById('about').addEventListener('click', openNav);
+document.getElementById('toggle-pt').addEventListener('click', () => translate('pt-br', 'Sobre'));
+document.getElementById('toggle-en').addEventListener('click', () => translate('en', 'About'));
 
-document.getElementById("closebtn").addEventListener("click", closeNav);
-document.getElementById("about").addEventListener("click", openNav);
-document.getElementById("pt").addEventListener("click", () => translate(json_pt));
-document.getElementById("en").addEventListener("click", () => translate(json_en));
 closeNav();
-
+translate('pt-br', 'Sobre');
 
 /* Open when someone clicks on the span element */
 function openNav() {
-  document.getElementById("myNav").style.display = "block";
+  document.getElementById('myNav').style.display = 'block';
 }
 
-/* Close when someone clicks on the "x" symbol inside the overlay */
+/* Close when someone clicks on the 'x' symbol inside the overlay */
 function closeNav() {
-  document.getElementById("myNav").style.display = "none";
+  document.getElementById('myNav').style.display = 'none';
 }
 
-function translate(json) {
-  for(key in json){
-    var value = json[key];
-    document.getElementById(key).innerText = value;
-  }
+function translate(id, about_text) {
+  hide_descriptions();
+  document.getElementById(id).style.display = 'block'
+  document.getElementById('about').innerText = about_text
+
+}
+
+function hide_descriptions() {
+  for (let el of document.querySelectorAll('.description')) el.style.display = 'none';
 }
