ff36a062861700e7c1066d454cc28e877d72bec1
Nathannael <nathannael2009@gmail.com>
2021-12-28 13:04:29 -0300

    Clean unused buttons and update text

--- js/main.js
+++ js/main.js
@@ -8,12 +8,12 @@ translate('pt-br', 'Sobre');
 
 /* Open when someone clicks on the span element */
 function openNav() {
-  document.getElementById('myNav').style.display = 'block';
+  document.getElementById('bio').style.display = 'block';
 }
 
 /* Close when someone clicks on the 'x' symbol inside the overlay */
 function closeNav() {
-  document.getElementById('myNav').style.display = 'none';
+  document.getElementById('bio').style.display = 'none';
 }
 
 function translate(id, about_text) {
